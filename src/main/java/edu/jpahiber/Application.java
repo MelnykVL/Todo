2969db208496de40d5aa3943b4ebb97e423b24b9
MelnykVL <melnykvladko@gmail.com>
2021-06-05 18:02:56 +0300

    Deleted "mustache" dependency and "templates" directory. Added "jstl" dependency in pom.xml.

--- src/main/java/edu/jpahiber/Application.java
+++ src/main/java/edu/jpahiber/Application.java
@@ -1,9 +1,5 @@
 package edu.jpahiber;
 
-import edu.jpahiber.model.Todo;
-import edu.jpahiber.model.User;
-import edu.jpahiber.service.TodoService;
-import edu.jpahiber.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
